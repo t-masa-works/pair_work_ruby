612573d3f1c0f2a068d0418d49ce72c5e8cfb6c5
tobe masa <t.masa.works@gmail.com>
2023-07-20 15:12:56 +0900

    ペアワーク

---
+++ janken.rb
@@ -0,0 +1,53 @@
+class Player
+  def hand
+    puts "0~2の数字を入力してください。"
+    puts "0:グー, 1:チョキ, 2:パー"
+
+    input_hand = gets.chomp
+
+    while true
+
+			if input_hand == "0" || input_hand == "1" || input_hand == "2"
+				 input_hand.to_i
+			else
+				puts "0~2の数字を入力してください。"
+				puts "0:グー, 1:チョキ, 2:パー"
+				input_hand = gets.chomp
+			end
+		end
+		player_hand = input_hand.to_i
+  end
+end
+# player = Player.new
+# puts player
+class Enemy
+	def hand
+		enemy_hand = rand(0..2)
+	end
+end
+
+class Janken
+	def pon(player_hand, enemy_hand)
+		janken = ["グー", "チョキ", "パー"]
+		puts "相手の手は#{janken[enemy_hand]}です。"
+		if player_hand == enemy_hand
+			puts "あいこ"
+		elsif (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
+			puts "あなたの勝ちです"
+		else
+			puts "あなたの負けです"
+		end
+	end
+end
+
+class GameStart
+	def self.jankenpon
+		player = Player.new
+		enemy = Enemy.new
+		janken = Janken.new
+		while next_game
+			next_game = janken.pon(player.hand, enemy.hand)
+		end
+	end
+end
+GameStart.jankenpon
